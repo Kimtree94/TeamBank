ce1473cdebeafebdfe5fe50bc1946209ef05b47c
dive27 <dive27@gmail.com>
2022-11-29 11:42:27 +0900

    테스트

--- src/main/java/com/Test/domain/Dto/Test2Dto.java
+++ src/main/java/com/Test/domain/Dto/Test2Dto.java
@@ -12,5 +12,5 @@ public class Test2Dto {
     private int store_no;
     private String store_name;
     private String store_nowday;
-    private String store_updateda;
+    private String store_updateday;
 }
